2681e201a26a9400e9648691b6f2196059160eff
Jon Krause <jon_krause@hotmail.com>
2025-04-26 10:31:37 -0400

    add library via code generation, not yaml

--- components/rtl_433/__init__.py
+++ components/rtl_433/__init__.py
@@ -34,6 +34,8 @@ CONFIG_SCHEMA = cv.Schema(
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
+    
+    cg.add_library("https://github.com/juanboro/rtl_433_Decoder_ESP.git",None)
 
     for conf in config.get(CONF_ON_JSON_MESSAGE, []):
         trigger = cg.new_Pvariable(conf[CONF_TRIGGER_ID])
